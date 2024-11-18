0bb53b086a6e12618f2610a6e27bf77a93067b25
JullerMus <juliusclykkegaard@gmail.com>
2024-11-18 14:58:24 +0100

    printing

---
+++ basics/Printing.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void world();
+
+int main() {
+    cout << "Hello" << endl;
+    world();
+
+    return 0;
+}
+
+void world() {
+    cout << "World!" << endl;
+}
