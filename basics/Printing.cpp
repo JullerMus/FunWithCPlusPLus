e35f4258638a7209697dee394c75a0e8bdda3ac0
JullerMus <juliusclykkegaard@gmail.com>
2024-11-18 15:06:57 +0100

    printing variables

--- basics/Printing.cpp
+++ basics/Printing.cpp
@@ -2,14 +2,21 @@
 using namespace std;
 
 void world();
+void printVariables(int a, int b);
 
 int main() {
-    cout << "Hello" << endl;
-    world();
+    /*cout << "Hello" << endl;
+    world();*/
+
+    printVariables(1, 2);
 
     return 0;
 }
 
 void world() {
     cout << "World!" << endl;
+}
+
+void printVariables(int a, int b) {
+    cout << "Printing a sum: " << a + b << endl;
 }
